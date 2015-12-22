35e33dc02ded778ef7a2e331c1eba07e511ace87
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-12-22 18:11:28 +0100

    demo: renamed template

--- demo/aping-config.js
+++ demo/aping-config.js
@@ -6,7 +6,7 @@ apingApp.config(['$provide', function ($provide) {
     });
 
     $provide.constant("apingDefaultSettings", {
-        templateUrl : "aping_design_sample.html",
+        templateUrl : "aping_design_blanko.html",
         items : 20, //items per request
         maxItems: 100, //max items per aping
         orderBy : "timestamp",
