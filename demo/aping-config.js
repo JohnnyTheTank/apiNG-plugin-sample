6b8bb74bdeaa1bdf45cf3d9efbadd071310588b7
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-01-24 00:14:20 +0100

    updated aping-config.js

--- demo/aping-config.js
+++ demo/aping-config.js
@@ -2,7 +2,6 @@
 angular.module('jtt_aping').config(['$provide', function ($provide) {
     $provide.value("apingDefaultSettings", {
         apingApiKeys : {
-            //...
             'sample': [
                 {'access_token':'<YOUR_API_TOKEN>'},
             ],
